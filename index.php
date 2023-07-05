82a7e244ed3e6346028f2e80ab4435e391577481
Said Lagauit <lagaouit.said@gmail.com>
2023-07-05 20:20:13 +0000

    updated

--- index.php
+++ index.php
@@ -26,6 +26,7 @@ if ($do == 'view') {
   $stmt = $con->prepare("SELECT `id`, `name`, `bio`, `pic` FROM `me` LIMIT 1");
   $stmt->execute();
   $me = $stmt->fetch(PDO::FETCH_ASSOC);
+
 ?>
 
   <div class="hero">
@@ -84,109 +85,42 @@ if ($do == 'view') {
   $slug = $_GET['slug'];
   $Articles = $con->prepare("SELECT `id`, `title`, `slug`, `cover`, `content`, `author`, `status_a`, `categories`, `tags`, `created`, `updated` FROM `articles` WHERE `slug` = ? AND `status_a` = '1'");
   $Articles->execute([$slug]);
-  if (!empty($Articles)) {
-    foreach ($Articles as $article) {
+  $post = $Articles->fetch(PDO::FETCH_ASSOC);
+  if (!$post) {
   ?>
-      <div class="row g-3 my-1">
-        <h2 class="title-reading text-capitalize">
-          <?php echo $article['title']; ?>
-        </h2>
-        <?php
-        if (!empty($article['cover'])) {
-        ?>
-          <div class="col-md-4">
-            <img class="img-reading" src="./uploads/<?php echo $article['cover']; ?>" alt="<?php echo $article['title']; ?>">
-          </div>
-        <?php
-        }
-        ?>
-        <div class="col-md-8 mx-auto">
-          <p>
-            <?php
-            $markdownContent = $article['content'];
-            $htmlContent = $Parsedown->text($markdownContent);
-            echo $htmlContent; ?>
-          </p>
-          <div class="comments-show border my-1 p-2">
-            <?php if (isset($_SESSION['message'])) : ?>
-              <div id="message">
-                <?php echo $_SESSION['message']; ?>
-              </div>
-              <?php unset($_SESSION['message']); ?>
-            <?php endif; ?>
-            <ul>
-              <?php
-              $articlesid = $article['id'];
-              $Allcomments = $con->prepare("SELECT comments.articlesid, comments.comment, comments.name, comments.approved, comments.date_c, articles.id FROM comments INNER JOIN articles ON comments.articlesid = articles.id WHERE comments.articlesid = ? ORDER BY comments.date_c DESC");
-              $Allcomments->execute(array($articlesid));
-              foreach ($Allcomments as $comments) {
-                $dateString = $comments['date_c'];
-                $now = date('Y-m-d H:i:s');
-                $timeDiff = strtotime($now) - strtotime($dateString);
-                if ($timeDiff > 86400) {
-                  $formattedDate = date('d-M', strtotime($dateString));
-                } else {
-                  $formattedDate = date('H:i', strtotime($dateString));
-                }
-                if ($comments['approved'] == 1) {
-              ?>
-                  <li>
-                    <h6>
-                      <?php echo $comments['name'] . '&nbsp;:&nbsp;<i class="fas fa-quote-left"></i>&nbsp;' . $comments['comment'] ?>&nbsp;
-                      <i class="fas fa-quote-right"></i></i>&nbsp;<span class="badge bg-dark">
-                        <?php echo $formattedDate; ?>
-                      </span>
-                    </h6>
-                  </li>
-              <?php
-                } else {
-                  echo '<p></p>';
-                }
-              }
-              ?>
-            </ul>
-          </div>
-          <form class="shadow p-2" action="index.php?do=comments-true" method="post" autocomplete="off" id="comments">
-            <div class="row g-3">
-              <div class="col-md-12">
-                <div class="form-group">
-                  <input type="hidden" name="articlesid" value="<?php echo $id; ?>">
-                  <label for="textarea" class="control-label">Comments<span class="text-danger">*</span></label>
-                  <textarea name="comment" class="form-control" rows="3" required="required"></textarea>
-                </div>
-              </div>
-              <div class="col-md-4">
-                <div class="form-group">
-                  <label class="control-label">Name<span class="text-danger">*</span></label>
-                  <input class="form-control" type="text" name="name" required="required" />
-                </div>
-              </div>
-              <div class="col-md-4">
-                <div class="form-group">
-                  <label class="control-label">Email<span class="text-danger">*</span></label>
-                  <input class="form-control" type="email" name="email" required="required" />
-                </div>
-              </div>
-              <div class="col-md-4">
-                <div class="form-group">
-                  <label class="control-label">Website</label>
-                  <input class="form-control" type="url" name="website" />
-                </div>
-              </div>
-              <div class="col-md-12">
-                <button class="btn btn-primary" type="submit" name="comments">
-                  <i class="fa fa-paper-plane"></i>&nbsp;Comment
-                </button>
-              </div>
-            </div>
-          </form>
-        </div>
+    <div class="col-md-8 mx-auto">
+      <div class="alert alert-warning text-center mt-2" role="alert">
+        Article not found
       </div>
-<?php
-    }
+    </div>
+  <?php
   } else {
-    header('Location: index.php');
-    exit();
+    $dateString = empty($post['updated']) ? $post['created'] : $post['updated'];
+    $now = date('Y-m-d H:i:s');
+    $timeDiff = strtotime($now) - strtotime($dateString);
+    if ($timeDiff > 86400) {
+      $Date = date('d-M', strtotime($dateString));
+    } else {
+      $Date = date('H:i', strtotime($dateString));
+    }
+    $cover = $post['cover'];
+    $title = $post['title'];
+    $markdownContent = $post['content'];
+    $content = $Parsedown->text($markdownContent);
+  ?>
+    <ul class="reading">
+      <li class="reading-cover text-bg-dark" style="background-image: url(./uploads/<?php echo $cover ?>);">
+        <h1 class="reading-title"><?php echo $title ?></h1>
+        <span class="reading-date"><?php echo $Date ?></span>
+      </li>
+      <li class="reading-content mt-4">
+        <div class="col-md-8 mx-auto">
+          <?php echo $content ?>
+        </div>
+      </li>
+      <li>add comment</li>
+    </ul>
+<?php
   }
 } elseif ($do == 'comments-true') {
   if (isset($_POST['comments'])) {
